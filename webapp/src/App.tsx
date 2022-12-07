949def0eba4efe25b04d8911a7b043c09810bec9
tyler <tyler@cruftbusters.com>
2022-12-07 15:51:06 -0800

    add all dummy fields

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,3 +1,60 @@
 export default function App() {
-  return <>hello world</>
+  return (
+    <>
+      <h2>Details</h2>
+      <div>
+        <Label htmlFor="projectName">Project Name:</Label>
+        <input name="projectName" />
+      </div>
+      <div>
+        <Label htmlFor="referenceNumber">Reference Number:</Label>
+        <input name="referenceNumber" />
+      </div>
+      <h2>Bill To</h2>
+      <div>
+        <Label htmlFor="billToLineOne">Line 1:</Label>
+        <input name="billToLineOne" />
+      </div>
+      <div>
+        <Label htmlFor="billToLineTwo">Line 2:</Label>
+        <input name="billToLineTwo" />
+      </div>
+      <div>
+        <Label htmlFor="billToLineThree">Line 3:</Label>
+        <input name="billToLineThree" />
+      </div>
+      <h2>Bill From</h2>
+      <div>
+        <Label htmlFor="billFromLineOne">Line 1:</Label>
+        <input name="billFromLineOne" />
+      </div>
+      <div>
+        <Label htmlFor="billFromLineTwo">Line 2:</Label>
+        <input name="billFromLineTwo" />
+      </div>
+      <div>
+        <Label htmlFor="billFromLineThree">Line 3:</Label>
+        <input name="billFromLineThree" />
+      </div>
+      <div>
+        <h2>Billing Period</h2>
+        <Label htmlFor="billingPeriodStart">Start:</Label>
+        <input name="billingPeriodStart" />
+        <Label htmlFor="billingPeriodEnd">End:</Label>
+        <input name="billingPeriodEnd" />
+      </div>
+      <div>
+        <Label htmlFor="hourlyRate">Hour Count:</Label>
+        <input name="hourCount" />
+      </div>
+      <div>
+        <Label htmlFor="hourlyRate">Hourly Rate:</Label>
+        <input name="hourlyRate" />
+      </div>
+    </>
+  )
+}
+
+function Label(props: JSX.IntrinsicElements['label']) {
+  return <label {...props} />
 }
