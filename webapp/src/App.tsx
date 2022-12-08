d51acaa7c92d65168a2423bc3fca3970cc3fae2f
tyler <tyler@cruftbusters.com>
2022-12-07 16:01:26 -0800

    add form layout

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,57 +1,58 @@
 export default function App() {
   return (
-    <>
-      <h2>Details</h2>
-      <div>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '10rem 20rem',
+        width: '30rem',
+        margin: '1rem auto',
+      }}
+    >
+      <>
+        <Header>Details</Header>
         <Label htmlFor="projectName">Project Name:</Label>
         <input name="projectName" />
-      </div>
-      <div>
-        <Label htmlFor="referenceNumber">Reference Number:</Label>
-        <input name="referenceNumber" />
-      </div>
-      <h2>Bill To</h2>
-      <div>
+        <Label htmlFor="hourlyRate">Hourly Rate:</Label>
+        <input name="hourlyRate" />
+      </>
+      <>
+        <Header>Bill To</Header>
         <Label htmlFor="billToLineOne">Line 1:</Label>
         <input name="billToLineOne" />
-      </div>
-      <div>
         <Label htmlFor="billToLineTwo">Line 2:</Label>
         <input name="billToLineTwo" />
-      </div>
-      <div>
         <Label htmlFor="billToLineThree">Line 3:</Label>
         <input name="billToLineThree" />
-      </div>
-      <h2>Bill From</h2>
-      <div>
+      </>
+      <>
+        <Header>Bill From</Header>
         <Label htmlFor="billFromLineOne">Line 1:</Label>
         <input name="billFromLineOne" />
-      </div>
-      <div>
         <Label htmlFor="billFromLineTwo">Line 2:</Label>
         <input name="billFromLineTwo" />
-      </div>
-      <div>
         <Label htmlFor="billFromLineThree">Line 3:</Label>
         <input name="billFromLineThree" />
-      </div>
-      <div>
-        <h2>Billing Period</h2>
+      </>
+      <>
+        <Header>Billing Period</Header>
+        <Label htmlFor="referenceNumber">Reference Number:</Label>
+        <input name="referenceNumber" />
         <Label htmlFor="billingPeriodStart">Start:</Label>
         <input name="billingPeriodStart" />
         <Label htmlFor="billingPeriodEnd">End:</Label>
         <input name="billingPeriodEnd" />
-      </div>
-      <div>
         <Label htmlFor="hourlyRate">Hour Count:</Label>
         <input name="hourCount" />
-      </div>
-      <div>
-        <Label htmlFor="hourlyRate">Hourly Rate:</Label>
-        <input name="hourlyRate" />
-      </div>
-    </>
+      </>
+    </div>
+  )
+}
+
+function Header({ children, ...props }: JSX.IntrinsicElements['h2']) {
+  return (
+    <h2 style={{ gridColumn: '1/3' }} {...props}>
+      {children}
+    </h2>
   )
 }
 
